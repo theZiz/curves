a60e8f309a297d1e427ba577660fc6620146af24
Ziz <zizsdl@googlemail.com>
2012-12-25 16:32:32 +0100

    Hermit De Casteljau

--- curves.c
+++ curves.c
@@ -17,11 +17,13 @@ typedef struct sPoint
 	pPoint next;
 } tPoint;
 
+#define TYPE_MAX 4
 typedef enum eType
 {
 	lagrange = 0,
 	hermit = 1,
-	bezier = 2
+	bezier = 2,
+	casteljau = 3
 } tType;
 
 tType type = lagrange;
@@ -30,6 +32,7 @@ pPoint firstPoint = NULL;
 pPoint  lastPoint = NULL;
 pPoint* pointArray;
 int pointCount;
+int hermitCount;
 float maxU = 0.0f;
 
 void resize( Uint16 w, Uint16 h )
@@ -98,6 +101,40 @@ float B_g_i(float t,int g,int i)
 	return g_over_i(g,i)*pow(1.0f-t,g-i)*pow(t,i);
 }
 
+float H_3_i(float t,int i)
+{
+	switch (i)
+	{
+		case 0: return (1.0f-t)*(1.0f-t)*(1.0f+2.0f*t);
+		case 1: return t*(1.0f-t)*(1.0f-t);
+		case 2: return -t*t*(1.0f-t);
+		case 3: return (3.0f-2.0f*t)*t*t;
+	}
+	return 0.0f;
+}
+
+float delta_i(int i)
+{
+	return pointArray[i+1]->u-pointArray[i]->u;
+}
+
+float s_i(float t,int i)
+{
+	return (t-pointArray[i]->u)/delta_i(i);
+}
+
+tPoint b_r_i(float t,int r,int i)
+{
+	if (r <= 0)
+		return (*pointArray[i]);
+	tPoint result,L,R;
+	L = b_r_i(t,r-1,i);
+	R = b_r_i(t,r-1,i+1);
+	result.x = (1.0f-t)*L.x+t*R.x;
+	result.y = (1.0f-t)*L.y+t*R.y;
+	result.z = (1.0f-t)*L.z+t*R.z;
+	return result;
+}
 
 void draw(void)
 {
@@ -133,13 +170,13 @@ void draw(void)
 	for (i = 1; i < pointCount; i++)
 		spLine3D(spFloatToFixed(pointArray[i-1]->x),spFloatToFixed(pointArray[i-1]->y),spFloatToFixed(pointArray[i-1]->z),
 						 spFloatToFixed(pointArray[i  ]->x),spFloatToFixed(pointArray[i  ]->y),spFloatToFixed(pointArray[i  ]->z),spGetFastRGB(127,127,127));
-	spSetAlphaTest(0);
   //curve
   float t;
   tPoint prev;
   switch (type)
   {
 		case lagrange:
+			spSetAlphaTest(0);
 			for (t = 0.0f; t <= maxU; t+=0.01f)
 			{
 				tPoint mom = {0.0f,0.0f,0.0f};
@@ -155,7 +192,83 @@ void draw(void)
 					         spFloatToFixed( mom.x),spFloatToFixed( mom.y),spFloatToFixed( mom.z),spGetFastRGB(255,0,255));
 				prev = mom;
 			}
+			spSetAlphaTest(1);
+			spFontDrawMiddle(screen->w>>1,2,-1,"Lagrange",font);
+		break;
 		case hermit:
+			for (i = 0; i < hermitCount; i++)
+				for (t = pointArray[i]->u; t < pointArray[(i+1)%hermitCount]->u; t+=0.01f)
+				{
+					tPoint mom,m1,m2;
+					if (i == 0)
+					{
+						m1.x = pointArray[(i+1)%hermitCount]->x - pointArray[(i+hermitCount-2)%hermitCount]->x;
+						m1.y = pointArray[(i+1)%hermitCount]->y - pointArray[(i+hermitCount-2)%hermitCount]->y;
+						m1.z = pointArray[(i+1)%hermitCount]->z - pointArray[(i+hermitCount-2)%hermitCount]->z;
+					}
+					else
+					if (i != pointCount)
+					{
+						m1.x = pointArray[(i+1)%hermitCount]->x - pointArray[(i+hermitCount-1)%hermitCount]->x;
+						m1.y = pointArray[(i+1)%hermitCount]->y - pointArray[(i+hermitCount-1)%hermitCount]->y;
+						m1.z = pointArray[(i+1)%hermitCount]->z - pointArray[(i+hermitCount-1)%hermitCount]->z;
+					}
+					else
+					{
+						m1.x = pointArray[(i+2)%hermitCount]->x - pointArray[(i+hermitCount-1)%hermitCount]->x;
+						m1.y = pointArray[(i+2)%hermitCount]->y - pointArray[(i+hermitCount-1)%hermitCount]->y;
+						m1.z = pointArray[(i+2)%hermitCount]->z - pointArray[(i+hermitCount-1)%hermitCount]->z;
+					}
+					/*float l = sqrt(m1.x*m1.x+m1.y*m1.y+m1.z*m1.z);
+					m1.x /= l;
+					m1.y /= l;
+					m1.z /= l;*/
+					if (i == 0)
+					{
+						m2.x = pointArray[(i+2)%hermitCount]->x - pointArray[(i+0)%hermitCount]->x;
+						m2.y = pointArray[(i+2)%hermitCount]->y - pointArray[(i+0)%hermitCount]->y;
+						m2.z = pointArray[(i+2)%hermitCount]->z - pointArray[(i+0)%hermitCount]->z;
+					}
+					else
+					if (i != pointCount)
+					{
+						m2.x = pointArray[(i+2)%hermitCount]->x - pointArray[(i+0)%hermitCount]->x;
+						m2.y = pointArray[(i+2)%hermitCount]->y - pointArray[(i+0)%hermitCount]->y;
+						m2.z = pointArray[(i+2)%hermitCount]->z - pointArray[(i+0)%hermitCount]->z;
+					}
+					else
+					{
+						m2.x = pointArray[(i+3)%hermitCount]->x - pointArray[(i+1)%hermitCount]->x;
+						m2.y = pointArray[(i+3)%hermitCount]->y - pointArray[(i+1)%hermitCount]->y;
+						m2.z = pointArray[(i+3)%hermitCount]->z - pointArray[(i+1)%hermitCount]->z;
+					}
+					/*l = sqrt(m2.x*m2.x+m2.y*m2.y+m2.z*m2.z);
+					m2.x /= l;
+					m2.y /= l;
+					m2.z /= l;*/
+					float f1 = H_3_i(s_i(t,i),0);
+					float f2 = delta_i(i) * H_3_i(s_i(t,i),1);
+					float f3 = delta_i(i) * H_3_i(s_i(t,i),2);
+					float f4 = H_3_i(s_i(t,i),3);
+					mom.x = f1 * pointArray[i]->x
+								+ f2 * m1.x
+								+ f3 * m2.x
+								+ f4 * pointArray[(i+1)%hermitCount]->x;
+					mom.y = f1 * pointArray[i]->y
+								+ f2 * m1.y
+								+ f3 * m2.y
+								+ f4 * pointArray[(i+1)%hermitCount]->y;
+					mom.z = f1 * pointArray[i]->z
+								+ f2 * m1.z
+								+ f3 * m2.z
+								+ f4 * pointArray[(i+1)%hermitCount]->z;
+					if (t!=0)
+						spLine3D(spFloatToFixed(prev.x),spFloatToFixed(prev.y),spFloatToFixed(prev.z),
+										 spFloatToFixed( mom.x),spFloatToFixed( mom.y),spFloatToFixed( mom.z),spGetFastRGB(127,127,255));
+					prev = mom;
+				}
+			spFontDrawMiddle(screen->w>>1,2,-1,"Hermite",font);
+		break;
 		case bezier:
 			for (t = 0.0f; t <= 1.0f; t+=0.01f)
 			{
@@ -172,11 +285,23 @@ void draw(void)
 					         spFloatToFixed( mom.x),spFloatToFixed( mom.y),spFloatToFixed( mom.z),spGetFastRGB(0,255,255));
 				prev = mom;
 			}
+			spFontDrawMiddle(screen->w>>1,2,-1,"Bezier",font);
+		break;
+		case casteljau:
+			for (t = 0.0f; t <= 1.0f; t+=0.01f)
+			{
+				tPoint mom = b_r_i(t,pointCount-1,0);
+				if (t!=0)
+					spLine3D(spFloatToFixed(prev.x),spFloatToFixed(prev.y),spFloatToFixed(prev.z),
+					         spFloatToFixed( mom.x),spFloatToFixed( mom.y),spFloatToFixed( mom.z),spGetFastRGB(127,255,127));
+				prev = mom;
+			}
+			spFontDrawMiddle(screen->w>>1,2,-1,"Casteljau",font);
 		break;
 	}
-	spSetAlphaTest(1);
-	spFontDrawMiddle(screen->w>>1,screen->h-font->maxheight-2,-1,"Press [R] to quit",font);
-	spFontDrawMiddle(screen->w>>1,2,-1,"Press [B] to pause",font);
+	spFontDraw(2,2,-1,"[q] before",font);
+	spFontDrawRight(screen->w-8,2,-1,"next [e]",font);
+	spFontDrawMiddle(screen->w>>1,screen->h-font->maxheight-2,-1,"Press [R] to quit, [B] to pause",font);
 	#ifdef SCALE_UP
 	spScale2XSmooth(screen,real_screen);
 	#endif
@@ -191,6 +316,16 @@ int calc(Uint32 steps)
 	{
 		pause = 1-pause;
 		engineInput->button[SP_BUTTON_SELECT] = 0;
+	}
+	if (engineInput->button[SP_BUTTON_L])
+	{
+		type = (type + TYPE_MAX - 1) % TYPE_MAX;
+		engineInput->button[SP_BUTTON_L] = 0;
+	}
+	if (engineInput->button[SP_BUTTON_R])
+	{
+		type = (type + 1) % TYPE_MAX;
+		engineInput->button[SP_BUTTON_R] = 0;
 	}
 	if (!pause)
 		rotation += steps*32;
@@ -248,7 +383,8 @@ int main(int argc, char **argv)
 		printf("Added point ( %6.2f | %6.2f | %6.2f )\n",point->x,point->y,point->z);
 		pointCount++;
 	}
-	pointArray = (pPoint*)malloc(sizeof(pPoint)*pointCount);
+	hermitCount = pointCount+1;
+	pointArray = (pPoint*)malloc(sizeof(pPoint)*hermitCount);
 	i = 0;
 	pPoint point = firstPoint;
 	while (point)
@@ -257,7 +393,12 @@ int main(int argc, char **argv)
 		i++;
 		point = point->next;
 	}
-	
+	pointArray[pointCount] = (pPoint)malloc(sizeof(tPoint));
+	pointArray[pointCount]->x = firstPoint->x;
+	pointArray[pointCount]->y = firstPoint->y;
+	pointArray[pointCount]->z = firstPoint->z;
+	pointArray[pointCount]->u = lastPoint->u + maxU / pointCount;
+	pointArray[pointCount]->next = NULL;
 	spSetDefaultWindowSize( 800, 480 );
 	spInitCore();
 	//Setup
